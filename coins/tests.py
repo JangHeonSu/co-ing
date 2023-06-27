d51b961677882e8c15984aec5bec95324c5bb771
J-malone <ekdepd2@gmail.com>
2023-06-28 02:32:45 +0900

    Remove .pyc files

--- coins/tests.py
+++ coins/tests.py
@@ -1,13 +1,40 @@
 from django.test import TestCase
 
+import jwt
+import hashlib
 import requests
-
-url = "https://api.upbit.com/v1/market/all"
-
-resp = requests.get(url)
-data = resp.json()
+import uuid
+from urllib.parse import urlencode, unquote
+from core.const import API_ACCESS_KEY, API_SECRET_KEY, API_UPBIT_SERVER_URL
 
 
-for coin in data:
-    ticker = coin['market']
-    print(ticker)
+print(API_ACCESS_KEY)
+print(API_SECRET_KEY)
+print(API_UPBIT_SERVER_URL)
+
+params = {
+  'currency': 'KRW',
+  'state': 'done'
+}
+query_string = unquote(urlencode(params, doseq=True)).encode("utf-8")
+
+m = hashlib.sha512()
+m.update(query_string)
+query_hash = m.hexdigest()
+
+payload = {
+    'access_key': API_ACCESS_KEY,
+    'nonce': str(uuid.uuid4()),
+}
+
+jwt_token = jwt.encode(payload, API_SECRET_KEY)
+authorization = 'Bearer {}'.format(jwt_token)
+print(authorization)
+headers = {
+  'Authorization': authorization,
+}
+
+res = requests.get(API_UPBIT_SERVER_URL + "/v1/account", headers=headers)
+print(res.json())
+# print(f"{currency_data['currency']} : {currency_data['balance']}")
+
---
+++ core/const.py
@@ -0,0 +1,5 @@
+import os
+
+API_ACCESS_KEY = os.environ.get('UPBIT_OPEN_API_ACCESS_KEY')
+API_SECRET_KEY = os.environ.get('UPBIT_OPEN_API_SECRET_KEY')
+API_UPBIT_SERVER_URL = "https://api.upbit.com"
